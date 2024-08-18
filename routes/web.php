31db48e8a2cb8b057e2c054e21a906e3f02d6a29
agorainitiative <info@agorainitiative.org>
2024-08-18 20:12:17 +0200

    route show articolo pubblica

--- routes/web.php
+++ routes/web.php
@@ -27,11 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('articles', ArticleController::class);
+    Route::resource('articles', ArticleController::class)->except('show');
 
 });
 
-
+Route::get('/article/{id}', [ArticleController::class, 'show'])->name('articles.show');
 
 Route::get('/checkout', function (Request $request) {
     $stripePriceId = 'price_deluxe_album';
